4d748ecde4569cd540c41c4e52963bdabfc9991e
Wolfgang Studier <wstudier@gmail.com>
2016-03-05 04:26:47 +0100

    more name refactoring, added getSize() methode to puf class, added genChallengeList to pufsim

--- pufsim.py
+++ pufsim.py
@@ -4,23 +4,6 @@ from abc import ABCMeta, abstractmethod
 import itertools
 import random
 
-#base class for random time generation of a single multiplexer
-class RNDBase(object):
-    
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def generateTimes(self):
-        pass
-        
-#Uniform distribution
-#generateTimes(self) returns 4 floats as tuple (Quadrupel?) 
-#Order of Values: up_up, down_down, up_down, down_up
-class RNDStuipdGeneator(RNDBase):
-    
-    def generateTimes(self):
-        return (random.random(),random.random(),random.random(),random.random())
-
 #single multiplexer
 class multiplexer(object):
     
@@ -50,6 +33,10 @@ class puf(object):
         for i in range(0, self.numOfMultip):
             tmp = multiplexer(gen.generateTimes())
             self.multiplexerList.append(tmp)
+
+    #returns puf size (number of multiplerxers)
+    def getSize(self):
+        return self.numOfMultip
     
     #single challenge to the puf
     #return: both times as tuple (up, down)
@@ -69,3 +56,41 @@ class puf(object):
     def challengeSingle(self, bitList):
         time_up, time_down = self.challenge(bitList)
         print '{' + ', '.join(map(str, bitList)) + '}\t' + str(time_up - time_down)
+
+#base class for random time generation of a single multiplexer
+class RNDBase(object):
+    
+    __metaclass__ = ABCMeta
+
+    #generateTimes(self) returns 4 floats as tuple (Quadrupel?) 
+    #Order of Values: up_up, down_down, up_down, down_up
+    @abstractmethod
+    def generateTimes(self):
+        pass
+        
+#Uniform distribution
+class RNDUniform(RNDBase):
+    
+    def generateTimes(self):
+        return (random.random(),random.random(),random.random(),random.random())
+
+#absolute standard normal distribution
+class RNDNormal(RNDBase):
+    
+    def generateTimes(self):
+        return (abs(random.normalvariate(0,1.0)),abs(random.normalvariate(0,1.0)),abs(random.normalvariate(0,1.0)),abs(random.normalvariate(0,1.0)))
+
+def genChallengeList(challengeSize, numOfChallenges):
+    if ((2 ** challengeSize) == numOfChallenges) :
+        return map(list, itertools.product([0, 1], repeat=challengeSize))
+    else :
+        challengeList = []
+        i = 0
+        while (i < numOfChallenges):
+            tmp = [random.randint(0,1) for b in range(0,challengeSize)]
+            if tmp not in challengeList:
+                challengeList.append(tmp)
+                i += 1
+        return challengeList
+        
+        
