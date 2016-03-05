3825cef5cbfcf7fab349af3014be01b5e8fdbf45
Wolfgang Studier <wstudier@gmail.com>
2016-03-05 16:54:30 +0100

    added example for pufEval class

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pufsim
 
 #create random generator instance
-rndgen = pufsim.RNDStuipdGeneator()
+rndgen = pufsim.RNDUniform()
 #create pufsim with 2 Multiplexer instances
 pufsimu = pufsim.puf(rndgen, 7)
 #do a single challenge to the pufsim with a challenge as list
@@ -10,9 +10,13 @@ pufsimu.challengeSingle([1,1,1,1,1,1,0])
 
 pufsimu.challengeSingle([0,1,1,1,1,1,1])
 
-#get a list of all challenges accordingly to the size of the pufsim (number of multiplexer)
-# Param: 0 Bit view 1: time difference view
-#pufsimu2 = pufsim.puf(rndgen, 5)
-#pufsimu2.challengeList(1)
+#worst case
+print len(pufsim.genChallengeList(10,((2**10)-1)))
+
+mutatio = pufsim.MutatorLastBitSwitch()
+
+tryEval = pufsim.pufEval(8, rndgen, 2**8, mutatio, 1000, 4)
+print "done"
+print tryEval.run()
 
 
