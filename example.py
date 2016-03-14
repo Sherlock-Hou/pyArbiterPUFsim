864fcbf511ce4cd7d21781e8868dc9da51e62dfa
Wolfgang Studier <wstudier@gmail.com>
2016-03-14 04:14:31 +0100

    added combinerFunc class and made them evaluable by pufEval class (readded utf-8 comment in example.py)

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pufsim
 import time
 
@@ -7,25 +9,27 @@ rndgen2 = pufsim.RNDNormal()
 #create pufsim with 2 Multiplexer instances
 pufsimu = pufsim.puf(rndgen, 7)
 #do a single challenge to the pufsim with a challenge as list
-pufsimu.challengeSingle([1,1,1,1,1,1,1])
-pufsimu.challengeSingle([1,1,1,1,1,1,0])
+pufsimu.challengePrint([1,1,1,1,1,1,1])
+pufsimu.challengePrint([1,1,1,1,1,1,0])
 
-pufsimu.challengeSingle([0,1,1,1,1,1,1])
+pufsimu.challengePrint([0,1,1,1,1,1,1])
 
 #worst case
 #print len(pufsim.genChallengeList(10,((2**10)-1)))
 
+combinerSimu = pufsim.simpleCombiner(rndgen2, 8)
+combinerSimu.challengePrint([0,1,1,1,1,1,1,1])
 
 
 mutatio = pufsim.MutatorLastBitSwitch()
 mutatio2 = pufsim.MutatorMiddleBitSwitch()
 
 
-
-tryEval = pufsim.pufEval(16, rndgen2, 2**8, mutatio, 1000, 4)
+#Aufruf hat sich verändert, zu testende Klasse muss übergeben werden.
+tryEval = pufsim.pufEval(pufsim.simpleCombiner, 16, rndgen2, 2**8, mutatio, 1000, 4)
 #tryEval.runStats()
 #tryEval.run()
-tryEval.runPlot(1)
+tryEval.runPlot(0)
 
 
 
