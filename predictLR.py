3cca113d18efb4b2ec684b345122b5abd1f55d5d
Nils Wisiol <info@nils-wisiol.de>
2016-03-15 12:49:41 +0100

    Logistic Regression with RPROP iteration
    Prediction rate for 64bit Arbiter PF approx. 80% when trained with
    640 key value pairs.

--- predictLR.py
+++ predictLR.py
@@ -2,78 +2,177 @@
 
 import pufsim
 from math import exp
-from numpy import around
-from scipy.spatial import distance
+from numpy import around, sign, zeros
+#from scipy.spatial import distance
 import sys
+from copy import copy
 
-# return the (standard) scalar product of two vectors x and y
-def sprod(x, y):
-    if len(x) != len(y):
-        raise RuntimeError('Both arguments must be vectors of the same length.')
-    return sum([ x[i]*y[i] for i in range(len(x)) ])
+class ArbiterLR():
 
-# logistic function
-def h(x, Θ):
-    return 1.0 / (1 + exp(-sprod(Θ, x)))
+    class triGen:
 
-# compute the new value for Θ[j]
-def iterateΘj(Θ, j, α, m, tSet):
-    return Θ[j] - α/m * sum([ tSet[i][0][j] * ( h(tSet[i][0], Θ) - tSet[i][1] ) for i in range(k)])
+        prev = None
+        cur = None
+        nxt = None
 
-# compute input product
-def inputProd(c):
-    return [
-        (-1)**sum([c[j] for j in range(i,k)])
-        for i in range(len(c))
-    ]
+        def __init__(self, default):
+            self.default = default
+            self.prev = copy(default)
+            self.cur = copy(default)
+            self.nxt = copy(default)
 
-# experiment parameters
-k = 32 # pf size
-m = 1000 # training set size
-n = 10000 # check set size
-convergeDecimals = 6 # number of decimals expected to be equal after one iteration
-maxTrainingIteration = 10e10
+        def iterate(self):
+            self.prev = self.cur
+            self.cur = self.nxt
+            self.nxt = copy(self.default)
 
-# create pufsim with k multiplexer instances
-arbiterpf = pufsim.puf(pufsim.RNDNormal(), k)
+    # return the (standard) scalar product of two vectors x and y
+    def sprod(self, x, y):
+        if len(x) != len(y):
+            raise RuntimeError('Both arguments must be vectors of the same length.')
+        return sum([ x[i]*y[i] for i in range(len(x)) ])
 
-# sample training set
-tChallenges = pufsim.genChallengeList(k, min([2**k, m]))
-tSet = [ ([1] + inputProd(c), (-1)**arbiterpf.challengeBit(c)) for c in tChallenges ]
+    # logistic function
+    def h(self, x, Θ):
+        p = -self.sprod(Θ, x)
+        try:
+            if p > 500:
+                # avoid overflow
+                return 0
+            return 1.0 / (1 + exp(p))
+        except OverflowError as e:
+            sys.stdout.write("\nh: overflow for p=" + str(p) + "x=" + str(["%8f" % e for e in x]) + ", Θ=" + str(["%8f" % e for e in Θ]))
+            raise e
 
-# train LR
-print("training...")
-converged = False
-i = 0
-α = 1.0
-Θ = [ 1 for x in range(k+1) ] # note k+1
-try:
-    while (not converged and i < maxTrainingIteration):
-        i += 1
-        oldΘ = Θ
-        Θ = [ iterateΘj(oldΘ, j, α, m, tSet) for j in range(k+1) ] # note k+1
-        converged = (around(oldΘ,decimals=convergeDecimals) == around(Θ,decimals=convergeDecimals)).all()
-        #print("Θ " + str(i) + "th iteration: " + str([ round(e,convergeDecimals) for e in Θ ]))
-        sys.stdout.write("\r" + str(i) + "th iteration -- current distance: " + str(round(distance.euclidean(oldΘ, Θ), convergeDecimals+2)))
+    # compute the new value for Θ[j]
+    def iterateΘj(self, Θ, j, α, m, tSet):
+        return Θ[j] - α/m * sum([ tSet[i][0][j] * ( self.h(tSet[i][0], Θ) - tSet[i][1] ) for i in range(k)])
 
-except(OverflowError):
-    print("OVERFLOW OCCURED, USING LAST KNOWN Θ")
+    # compute input product
+    def inputProd(self, c):
+        return [
+            (-1)**sum([c[j] for j in range(i,self.k)])
+            for i in range(len(c))
+        ]
 
-# assess quality
-print("checking...")
-cChallenges = pufsim.genChallengeList(k, min([2**k, n]))
-good = 0
-bad = 0
-for c in cChallenges:
-    pfResponse = arbiterpf.challengeBit(c)
-    lrResponse = 1 if sprod(Θ, [1] + inputProd(c)) < 0 else 0
-    if pfResponse == lrResponse:
-        good += 1
-    else:
-        bad += 1
+    def __init__(self, k, m, M, n):
+        # experiment parameters
+        self.k = k # pf size
+        self.m = min([2**k, m]) # training set size
+        self.M = M # number of wrong CRPs
+        self.n = min([2**k, n]) # check set size
+        self.convergeDecimals = 8 # number of decimals expected to be equal after one iteration
+        self.maxTrainingIteration = 100
 
-print("Learned delay values Θ: " + str([round(e,3) for e in Θ]))
-print("Predicted correctly: " + str(good))
-print("          falsely: " + str(bad))
-print("Prediction rate: " + str(float(good)/float(good+bad)))
-print("Terminated after " + str(i) + " iterations.")
+        # create pufsim with k multiplexer instances
+        self.arbiterpf = pufsim.puf(pufsim.RNDNormal(), k)
+
+    def train(self):
+        # sample training set
+        tChallenges = pufsim.genChallengeList(self.k, self.m + self.M)
+        # add correct challenges
+        tSet = [ ([1] + self.inputProd(c), self.arbiterpf.challengeBit(c)) for c in tChallenges[:self.m] ]
+        # add wrong challenges
+        tSet += [ ([1] + self.inputProd(c), 1-self.arbiterpf.challengeBit(c)) for c in tChallenges[self.m:] ]
+
+        converged = False
+        i = 0
+
+        # RPROP parameters
+        ηplus = 1.2
+        ηminus = 0.5
+        Δmin = 10**-6
+        Δmax = 50
+
+        # learned delay values
+        Θ = self.triGen([ 1 for x in range(self.k+1) ]) # note k+1
+
+        # partial derivatives of error function
+        pE = self.triGen(default=[ 0 for x in range(self.k+1) ])
+
+        # update values for Θ
+        Δ = self.triGen(default=[ 0 for x in range(self.k+1) ]) # note k+1
+        Δ.cur = [ 1 for x in range(self.k+1) ] # init for first iteration
+        ΔΘ = self.triGen(default=[ 0 for x in range(self.k+1) ]) # note k+1
+
+        try:
+            while (not converged and i < self.maxTrainingIteration):
+                # count iterations
+                i += 1
+
+                # compute new Θ (RPROP algorithm)
+                for j in range(self.k+1):
+                    # compute pE.cur[j]
+                    pE.cur[j] = 1/float(self.m+self.M) * sum([ tSet[i][0][j] * ( self.h(tSet[i][0], Θ.cur) - tSet[i][1] ) for i in range(self.m+self.M)])
+
+                    # compute Θ.nxt[j]
+                    if pE.prev[j]*pE.cur[j] > 0:
+                        Δ.cur[j] = min([Δ.prev[j] * ηplus, Δmax])
+                        ΔΘ.cur[j] = -sign(pE.cur[j]) * Δ.cur[j]
+                        Θ.nxt[j] = Θ.cur[j] + ΔΘ.cur[j]
+
+                    elif pE.prev[j]*pE.cur[j] < 0:
+                        Δ.cur[j] = max([Δ.prev[j] * ηminus, Δmin])
+                        Θ.nxt[j] = Θ.cur[j] - ΔΘ.prev[j]
+                        pE.cur[j] = 0
+
+                    elif pE.prev[j]*pE.cur[j] == 0:
+                        ΔΘ.cur[j] = -sign(pE.cur[j]) * Δ.cur[j]
+                        Θ.nxt[j] = Θ.cur[j] + ΔΘ.cur[j]
+
+                sys.stdout.write(" ")
+
+                # iterate the triGens Δ, ΔΘ, Θ, pE
+                Δ.iterate()
+                ΔΘ.iterate()
+                Θ.iterate()
+                pE.iterate()
+
+                # check for convergence
+                converged = (around(Θ.prev,decimals=self.convergeDecimals) == around(Θ.cur,decimals=self.convergeDecimals)).all()
+                #sys.stdout.write("Θ(" + str(i) + "): " + str(["%8f" % e for e in Θ.cur]) + "; ")
+                #sys.stdout.write(str(i) + "th iteration -- current distance: " + str(round(distance.euclidean(Θ.prev, Θ.cur), convergeDecimals+2)) + "\n")
+
+        except OverflowError as e:
+            #print()
+            print("OVERFLOW OCCURED, USING LAST KNOWN Θ [" + str(e) + "]")
+            Θ.cur = Θ.prev
+
+        self.Θ = Θ.cur
+        return self.Θ
+
+    def check(self):
+        # assess quality
+        cChallenges = pufsim.genChallengeList(self.k, self.n)
+        good = 0
+        bad = 0
+        for c in cChallenges:
+            pfResponse = self.arbiterpf.challengeBit(c)
+            lrResponse = 0 if self.sprod(self.Θ, [1] + self.inputProd(c)) < 0 else 1
+            if pfResponse == lrResponse:
+                good += 1
+            else:
+                bad += 1
+                #print("got " + str(lrResponse) + " but expected " + str(pfResponse))
+
+        return float(good)/float(good+bad)
+
+    def run(self):
+        self.train()
+        return self.check()
+
+result = zeros((10, 6, 10))
+for iIdx in range(0, 10):
+    for mIdx in range(0, 10):
+        m = 100 * (mIdx+1)
+        for MIdx in range(6):
+            try:
+                M = int(m * (MIdx/10.0))
+                lr = ArbiterLR(k=64, m=m, M=M, n=10000)
+                sys.stdout.write("%s,%s,%s,%s,%s," % (m,M,mIdx,MIdx,iIdx))
+                result[mIdx][MIdx][iIdx] = lr.run()
+                sys.stdout.write("%s\n" % result[mIdx][MIdx][iIdx])
+            except Exception as e:
+                sys.stdout.write("%s\n" % str(e))
+
+            #sys.stderr.write(str(result) + "\n\n###################\n\n")
